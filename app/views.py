c6ae2e092526b8f10aa1c5c658723e0d18c75bf5
^_____^ Kayla <kramos577@gmail.com>
2017-06-19 20:22:00 -0700

    Added Subheaders and changed logo

--- app/views.py
+++ app/views.py
@@ -7,22 +7,8 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'nickname': 'The Awesome El Jesso'}  # fake user
-    posts = [  # fake array of posts
-        {
-            'author': {'nickname': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'nickname': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
     return render_template("index.html",
-                           title='Home',
-                           user=user,
-                           posts=posts)
-
+                           title='Home')
 
 @app.route('/lessons')
 def lessons():

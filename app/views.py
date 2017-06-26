c5d4f89f1be3db98ce4860fba5410981b360c478
^_____^ Kayla <kramos577@gmail.com>
2017-06-26 14:29:51 -0700

    Ch 1 vocab with and print test

--- app/views.py
+++ app/views.py
@@ -1,8 +1,14 @@
+#   encoding: utf-8
 from flask import render_template
 #This function takes a template filename and a variable list of template arguments and returns the rendered template, with all the arguments replaced.
 #Under the covers, the render_template function invokes the Jinja2 templating engine that is part of the Flask framework. Jinja2 substitutes {{...}}
 #blocks with the corresponding values provided as template arguments.
+
+#first line of any file with Japanese = (tab)encoding: utf-16    and then put u'char' everywhere
+#all ... are ~     and words with more than one meanign are in an array    and there are no capital letters excpet the pronoun I 　　and　katakana is in hiragana
 from app import app
+from ch1vocab import ch1V
+
 
 @app.route('/')
 @app.route('/index')
@@ -13,14 +19,16 @@ def index():
 @app.route('/lessons')
 def lessons():
     return render_template("lessons.html",
-                           title='lessons')
+                           title='Lessons')
 
 @app.route('/kanji')
 def kanji():
     return render_template("kanji.html",
-                           title='kanji')
+                           title='Kanji')
+
 
 @app.route('/vocab')
 def vocab():
     return render_template("vocab.html",
-                           title='Vocabulary')
+                           title='Vocabulary',
+                           chap=ch1V) #appending is list1 + list2 and just appends in order

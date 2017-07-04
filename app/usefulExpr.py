dd3ac9a8e373e097267997547a30945a9a9fbf6c
^_____^ Kayla <kramos577@gmail.com>
2017-07-03 18:20:08 -0700

    Ch 3 vocab

---
+++ app/usefulExpr.py
@@ -0,0 +1,9 @@
+# encoding: utf-8
+expr = [{'word':'do you understand', 'romaji':'wakarimashita ka', 'kana': u'', 'hiragana': u''},
+        {'word':['i understand','i understood'], 'romaji':'wakarimashita', 'kana': u'', 'hiragana': u''},
+        {'word':['i don\'t understand','i don\'t know'], 'romaji':'wakarimasen', 'kana': u'', 'hiragana': u''},
+        {'word':'please say it slowly', 'romaji':'yukkuri itte kudasai', 'kana': u'', 'hiragana': u''},
+        {'word':'please say it again', 'romaji':'mou ichido itte kudasai', 'kana': u'', 'hiragana': u''},
+        {'word':'please wait', 'romaji':'chotto matte kudasai', 'kana': u'', 'hiragana': u''},
+        {'word':['please listen','please ask'], 'romaji':'kiite kudasai', 'kana': u'', 'hiragana': u''},
+        {'word':'please look at page 10', 'romaji':'juppeeji wo mite kudasai', 'kana': u'', 'hiragana': u''}]
